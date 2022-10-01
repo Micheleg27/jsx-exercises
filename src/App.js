1dd7c33d00288fea9c103c458c76d71d8e9115fe
Micheleg27 <michele.garribba27@github.com>
2022-10-01 16:01:04 +0200

    fixed exe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Par from './HelloWorld';
+import Hed from './HelloWorld';
 
 function App() {
   return (
-    <Par/>
+    <Par />
   );
 }
 
